c9794ccf51e13b7b18fd0d189d2b3ad4daf7484c
an-asa <pantheyan@gmail.com>
2020-05-28 19:40:29 +0200

    Added right-click button creation in window

--- app.py
+++ app.py
@@ -1,9 +1,36 @@
 from kivy.app import App
-from kivy.uix.widget import Widget
+from kivy.config import Config
 
-class DemoWidget(Widget):
-    pass
+Config.set('graphics', 'resizable', False)
+Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+from kivy.core.window import Window
+
+
+class Demoer(FloatLayout):
+
+    def __init__(self, *args, **kwargs):
+        super(Demoer, self).__init__(*args, **kwargs)
+        nodebutton = Button(pos=(100, 100), size_hint=(None, None), size=(10, 10))
+        nodebutton.bind(on_press=self.clicknode)
+        self.add_widget(nodebutton)
+
+    def clicknode(self, instance):
+        self.remove_widget(instance)
+
+    def on_touch_down(self, touch):
+        if self.collide_point(touch.pos[0], touch.pos[1]):
+            if touch.button == "right":
+                nodebutton = Button(pos=(touch.x - 5, touch.y - 5), size_hint=(None, None), size=(10, 10))
+                nodebutton.bind(on_press=self.clicknode)
+                self.add_widget(nodebutton)
+                return True
+
 
 class DemoApp(App):
     def build(self):
-        return DemoWidget()
+        Window.size = (800, 600)
+        return Demoer()
