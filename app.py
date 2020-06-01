73a1d47e487d07d336979b248d0ffe9dbfd15c38
an-asa <pantheyan@gmail.com>
2020-06-01 21:40:35 +0200

    Added comments and node type icons

--- app.py
+++ app.py
@@ -10,15 +10,17 @@ Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
-from kivy.uix.widget import Widget
+from manager import NetworkManager
 from kivy.core.window import Window
 
 
 class Demoer(FloatLayout):
     # multiple choice bubble menu for node addition
-    defaultBubble = Bubble(size_hint=(None, None), size=(100, 40))
-    newNodeButton = Button(text="Nowy węzeł")
-    defaultBubble.add_widget(newNodeButton)
+    defaultBubble = Bubble(size_hint=(None, None), size=(250, 40))
+    newComputerButton = Button(text="Nowy komputer")
+    newRouterButton = Button(text="Nowy router")
+    defaultBubble.add_widget(newComputerButton)
+    defaultBubble.add_widget(newRouterButton)
 
     # coordinates of node-button to be placed
     pendingNodePosX = 0
@@ -34,7 +36,10 @@ class Demoer(FloatLayout):
     # reference to button being edited/removed
     pendingNodeRef = Button()
 
+    # global state - adding connection or not
     isInConnectionMode = False
+
+    netManager = NetworkManager()
 
     def __init__(self, *args, **kwargs):
         # set window color
@@ -60,24 +65,45 @@ class Demoer(FloatLayout):
     def deleteNode(self, instance):
         self.clearBubbles()
         self.remove_widget(self.pendingNodeRef)
+        self.netManager.deleteNode(self.pendingNodeRef)
 
-    # add pending node after clicking in bubble menu
-    def addNode(self, instance):
+    # add pending node as computer after clicking in bubble menu
+    def addComputer(self, instance):
         nodeButton = Button(pos=(self.pendingNodePosY, self.pendingNodePosX), size_hint=(None, None), size=(40, 40))
-        nodeImg = Image(source="Images/router.png")
-        nodeLabel = Label(text="PC1")
+        nodeImg = Image(source="Images/computer.png")
+        nodeLabel = Label()
         nodeButton.add_widget(nodeImg)
         nodeButton.add_widget(nodeLabel)
         nodeImg.center_x = nodeImg.parent.center_x
         nodeImg.center_y = nodeImg.parent.center_y+10
         nodeLabel.center_x = nodeLabel.parent.center_x
         nodeLabel.center_y = nodeLabel.parent.center_y-10
-        nodeButton.bind(on_press=self.showNodebubble)
+        nodeButton.bind(on_press=self.showNodeBubble)
+        nodeButton.type = "Computer"
         self.add_widget(nodeButton)
+        self.netManager.addComputer(nodeButton)
+        nodeLabel.text = "PC"
+
+    # add pending node as router after clicking in bubble menu
+    def addRouter(self, instance):
+        nodeButton = Button(pos=(self.pendingNodePosY, self.pendingNodePosX), size_hint=(None, None), size=(40, 40))
+        nodeImg = Image(source="Images/router.png")
+        nodeLabel = Label()
+        nodeButton.add_widget(nodeImg)
+        nodeButton.add_widget(nodeLabel)
+        nodeImg.center_x = nodeImg.parent.center_x
+        nodeImg.center_y = nodeImg.parent.center_y+10
+        nodeLabel.center_x = nodeLabel.parent.center_x
+        nodeLabel.center_y = nodeLabel.parent.center_y-10
+        nodeButton.bind(on_press=self.showNodeBubble)
+        nodeButton.type = "Router"
+        self.add_widget(nodeButton)
+        self.netManager.addRouter(nodeButton)
+        nodeLabel.text = "R"
 
     # show bubble menu on click on node
     # OR create connection between active node and clicked node when in connection mode
-    def showNodebubble(self, instance):
+    def showNodeBubble(self, instance):
         if not self.isInConnectionMode:
             self.clearBubbles()
             self.pendingNodeRef = instance
@@ -86,18 +112,22 @@ class Demoer(FloatLayout):
             self.newConnButton.bind(on_press=self.toggleConnectionMode)
             self.add_widget(self.nodeBubble)
         else:
-            with self.canvas.before:
-                Line(points=[self.pendingNodeRef.pos[0] + 20, self.pendingNodeRef.pos[1] + 20,
-                             instance.pos[0] + 20, instance.pos[1] + 20], width=2)
-            self.toggleConnectionMode(Button())
+            if (instance.type == "Router" and self.pendingNodeRef.type == "Computer") or \
+                    (self.pendingNodeRef.type == "Router" and instance.type == "Computer") or \
+                    (self.pendingNodeRef.type == "Router" and instance.type == "Router"):
+                with self.canvas.before:
+                    Line(points=[self.pendingNodeRef.pos[0] + 20, self.pendingNodeRef.pos[1] + 20,
+                                 instance.pos[0] + 20, instance.pos[1] + 20], width=2)
+                self.toggleConnectionMode(Button())
 
     # show bubble on right-clicking canvas
     def showDefaultBubble(self, posx, posy):
         self.clearBubbles()
         self.pendingNodePosY = posx - 20
         self.pendingNodePosX = posy - 20
-        self.defaultBubble.pos = (posx - 50, posy)
-        self.newNodeButton.bind(on_press=self.addNode)
+        self.defaultBubble.pos = (posx - 125, posy)
+        self.newComputerButton.bind(on_press=self.addComputer)
+        self.newRouterButton.bind(on_press=self.addRouter)
         self.add_widget(self.defaultBubble)
 
     def on_touch_down(self, touch, after=False):
