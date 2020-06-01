2444e12db199ce398eeb3e28af4eeca8b30055d8
an-asa <pantheyan@gmail.com>
2020-06-01 20:39:16 +0200

    Added comments and node type icons

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from kivy.clock import Clock
 from kivy.config import Config
 from kivy.graphics.vertex_instructions import Line
 from kivy.uix.bubble import Bubble
+from kivy.uix.image import Image
 
 Config.set('graphics', 'resizable', False)
 Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
@@ -14,28 +15,33 @@ from kivy.core.window import Window
 
 
 class Demoer(FloatLayout):
-
+    # multiple choice bubble menu for node addition
     defaultBubble = Bubble(size_hint=(None, None), size=(100, 40))
     newNodeButton = Button(text="Nowy węzeł")
     defaultBubble.add_widget(newNodeButton)
 
+    # coordinates of node-button to be placed
     pendingNodePosX = 0
     pendingNodePosY = 0
 
+    # multiple choice bubble menu for node editing/removal
     nodeBubble = Bubble(size_hint=(None, None), size=(250, 40))
     newConnButton = Button(text="Dodaj połączenie")
     deleteNodeButton = Button(text="Usuń węzeł")
     nodeBubble.add_widget(newConnButton)
     nodeBubble.add_widget(deleteNodeButton)
 
+    # reference to button being edited/removed
     pendingNodeRef = Button()
 
     isInConnectionMode = False
 
     def __init__(self, *args, **kwargs):
-        Window.clearcolor=(0.7, 0.7, 0.7, 1)
+        # set window color
+        Window.clearcolor = (0.7, 0.7, 0.7, 1)
         super(Demoer, self).__init__(*args, **kwargs)
 
+    ## switch between placing connection or editing nodes
     def toggleConnectionMode(self, instance):
         self.clearBubbles()
         if not self.isInConnectionMode:
@@ -45,38 +51,52 @@ class Demoer(FloatLayout):
             self.pendingNodeRef.background_color = (1, 1, 1, 1)
             self.isInConnectionMode = False
 
+    # remove active bubble menus
     def clearBubbles(self):
         self.remove_widget(self.nodeBubble)
         self.remove_widget(self.defaultBubble)
 
+    # remove active node after clicking in bubble menu
     def deleteNode(self, instance):
         self.clearBubbles()
         self.remove_widget(self.pendingNodeRef)
 
+    # add pending node after clicking in bubble menu
     def addNode(self, instance):
-        nodebutton = Button(pos=(self.pendingNodePosY, self.pendingNodePosX), size_hint=(None, None), size=(40, 40))
-        nodebutton.bind(on_press=self.showNodebubble)
-        self.add_widget(nodebutton)
+        nodeButton = Button(pos=(self.pendingNodePosY, self.pendingNodePosX), size_hint=(None, None), size=(40, 40))
+        nodeImg = Image(source="Images/router.png")
+        nodeLabel = Label(text="PC1")
+        nodeButton.add_widget(nodeImg)
+        nodeButton.add_widget(nodeLabel)
+        nodeImg.center_x = nodeImg.parent.center_x
+        nodeImg.center_y = nodeImg.parent.center_y+10
+        nodeLabel.center_x = nodeLabel.parent.center_x
+        nodeLabel.center_y = nodeLabel.parent.center_y-10
+        nodeButton.bind(on_press=self.showNodebubble)
+        self.add_widget(nodeButton)
 
+    # show bubble menu on click on node
+    # OR create connection between active node and clicked node when in connection mode
     def showNodebubble(self, instance):
         if not self.isInConnectionMode:
             self.clearBubbles()
             self.pendingNodeRef = instance
-            self.nodeBubble.pos = (instance.pos[0]-105, instance.pos[1]+40)
+            self.nodeBubble.pos = (instance.pos[0] - 105, instance.pos[1] + 40)
             self.deleteNodeButton.bind(on_press=self.deleteNode)
             self.newConnButton.bind(on_press=self.toggleConnectionMode)
             self.add_widget(self.nodeBubble)
         else:
             with self.canvas.before:
-                line=Line(points=[self.pendingNodeRef.pos[0]+20, self.pendingNodeRef.pos[1]+20,
-                             instance.pos[0]+20, instance.pos[1]+20], width=2)
+                Line(points=[self.pendingNodeRef.pos[0] + 20, self.pendingNodeRef.pos[1] + 20,
+                             instance.pos[0] + 20, instance.pos[1] + 20], width=2)
             self.toggleConnectionMode(Button())
 
+    # show bubble on right-clicking canvas
     def showDefaultBubble(self, posx, posy):
         self.clearBubbles()
         self.pendingNodePosY = posx - 20
         self.pendingNodePosX = posy - 20
-        self.defaultBubble.pos = (posx-50, posy)
+        self.defaultBubble.pos = (posx - 50, posy)
         self.newNodeButton.bind(on_press=self.addNode)
         self.add_widget(self.defaultBubble)
 
