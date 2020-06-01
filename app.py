80567c9a723d3f4ecff02298e82df38ae773e730
an-asa <pantheyan@gmail.com>
2020-06-01 11:08:12 +0200

    Added right-click button creation in window

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from kivy.app import App
+from kivy.clock import Clock
 from kivy.config import Config
-
 Config.set('graphics', 'resizable', False)
 Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
 from kivy.uix.floatlayout import FloatLayout
@@ -14,20 +14,20 @@ class Demoer(FloatLayout):
 
     def __init__(self, *args, **kwargs):
         super(Demoer, self).__init__(*args, **kwargs)
-        nodebutton = Button(pos=(100, 100), size_hint=(None, None), size=(10, 10))
-        nodebutton.bind(on_press=self.clicknode)
-        self.add_widget(nodebutton)
 
     def clicknode(self, instance):
-        self.remove_widget(instance)
+        Clock.schedule_once(lambda dt: self.remove_widget(instance), 0.2)
 
-    def on_touch_down(self, touch):
-        if self.collide_point(touch.pos[0], touch.pos[1]):
+    def on_touch_down(self, touch, after=False):
+        if after and self.collide_point(touch.pos[0], touch.pos[1]):
             if touch.button == "right":
-                nodebutton = Button(pos=(touch.x - 5, touch.y - 5), size_hint=(None, None), size=(10, 10))
+                nodebutton = Button(pos=(touch.x - 20, touch.y - 20), size_hint=(None, None), size=(40, 40))
                 nodebutton.bind(on_press=self.clicknode)
                 self.add_widget(nodebutton)
                 return True
+        else:
+            Clock.schedule_once(lambda dt: self.on_touch_down(touch, True), 0.01)
+            return super(Demoer, self).on_touch_down(touch)
 
 
 class DemoApp(App):
