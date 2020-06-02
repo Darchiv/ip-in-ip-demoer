517f31d21aa510d73f9c08e1eb5a9279dd232cfe
Jakub Krawczuk <kubkra@gmail.com>
2020-06-02 17:26:33 +0200

    encap functionality implementation

--- Packet.py
+++ Packet.py
@@ -13,7 +13,7 @@ class Packet:
     # no options - packet header size 5 * 32bit
     IHL = 5
 
-    header_size = IHL * 32 / 8
+    header_size = int(IHL * 32 / 8)
 
     # DSCP - service type - copy from oryginal
     # ECN - end-to-end notification - copy?
@@ -42,21 +42,28 @@ class Packet:
 
     def toData(self):
         header = [self.VERSION, self.IHL, self.dscp,
-                  int(self.header_size + len(self.data)), self.id, "00"+str(self.flag2),
+                  int(self.header_size + len(self.data)), self.id, "00" + str(self.flag2),
                   self.offset, self.ttl, self.protocol, "checksum", self.source, self.destination]
         result = str(header)
         result += self.data
-        return "{"+result+"}"
+        return "{" + result + "}"
 
     def encap(self, source, destination, protocol=Protocol.UDP,
-                 ttl=15, offset=0, flag2=0, uid=0, dscp=0, max_size=30):
+              ttl=15, offset=0, flag2=0, uid=0, dscp=0, max_size=30):
         packet_size = self.header_size + len(self.data)
         if packet_size > self.MAXSIZE:
-            print("wymagany podział na osobne datagramy (id??)")
+            # larger than max datagram size
+            p1 = Packet(source, destination, data=self.toData()[0:max_size - self.MAXSIZE])
+            p2 = Packet(source, destination, data=self.toData()[max_size - self.MAXSIZE:],
+                        uid=self.id+1)
+            return [p1, p2]
+
         elif packet_size > max_size:
-
-            print("Wymagany podział na pakiety (offset??)")
+            # larger than network settings
+            # todo take into account there may be need for more than 2 packets
+            p1 = Packet(source, destination, data=self.toData()[0:max_size - self.header_size])
+            p2 = Packet(source, destination, data=self.toData()[max_size - self.header_size:],
+                        offset=(max_size - self.header_size))
+            return [p1, p2]
         else:
-            print("Zmiesci się w 1 pakiecie")
-            return Packet(source, destination, data=self.toData())
-
+            return [Packet(source, destination, data=self.toData())]
--- test.py
+++ test.py
@@ -3,9 +3,10 @@ from Packet import Packet
 print("test GitHub push")
 # testing pushing via PyCharm -JZ
 
-p = Packet("X", "Y", data="xyz")
+p = Packet("X", "Y", data="xyzasdasdasdasdasda")
 print(p.toData())
 
-x = p.encap("A", "B")
-print(x.toData())
+x = p.encap("A", "B", max_size=25)
+for p in x:
+    print(p.toData())
 
